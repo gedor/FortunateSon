404032d53c4d230bbffbf7567f6f3986a63071da
DESKTOP-CU92D4B\lovro <gedgedor@gmail.com>
2018-06-22 23:49:06 +0200

    Dodao novi cursor, ikone logo i dodao rubove na neke Canvase.

---
+++ Assets/Scripts/Managers/CursorManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour {
+
+
+	public CursorMode cursorMode = CursorMode.Auto;
+	public Vector2 hotspot = Vector2.zero;
+	public Texture2D cursorTexture;
+	// Use this for initialization
+	void Awake () {
+		GameObject.DontDestroyOnLoad(this);
+		Cursor.SetCursor(cursorTexture,hotspot,cursorMode);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
